7b4c8c24797c79a2d291a4e6645edbe3f4269901
Fabricio Garcia <gael.fabricio@gmail.com>
2020-07-27 14:44:33 -0500

    feat: Modify physics interaction with player in Game scene

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -33,8 +33,11 @@ export default class Game extends Phaser.Scene {
     }
 
     this.player = this.physics.add.sprite(240, 320, 'bunny-stand').setScale(0.5);
-
     this.physics.add.collider(platforms, this.player);
+    this.player.body.checkCollision.up = false;
+    this.player.body.checkCollision.left = false;
+    this.player.body.checkCollision.right = false;
+    this.cameras.main.startFollow(this.player);
   }
 
   update() {
