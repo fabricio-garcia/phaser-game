fa2657b26e08ad9a6a40f711dc79f0c0717eef37
Fabricio Garcia <gael.fabricio@gmail.com>
2020-07-27 12:57:20 -0500

    feat: Add 'platform' sprite to Game scene

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,5 +1,6 @@
 import Phaser from 'phaser';
 import Layer from '../assets/bg_layer1.png';
+import Platform from '../assets/ground_grass.png';
 
 export default class Game extends Phaser.Scene {
   constructor() {
@@ -8,9 +9,11 @@ export default class Game extends Phaser.Scene {
 
   preload() {
     this.load.image('background', Layer);
+    this.load.image('platform', Platform);
   }
 
   create() {
     this.add.image(240, 320, 'background');
+    this.add.image(240, 320, 'platform').setScale(0.5);
   }
 }
