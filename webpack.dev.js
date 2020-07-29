1ce6ddcd66090108e1e12dcd16a67bf05d762105
Fabricio Garcia <gael.fabricio@gmail.com>
2020-07-29 12:55:40 -0500

    fix: Linting errors

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const path = require('path');
-const webpack = require('webpack');
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const common = require('./webpack.common');
